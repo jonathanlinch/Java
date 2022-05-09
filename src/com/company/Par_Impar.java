434eecbc4d7f3bbae61b26539d7e8c54ba2b3431
Jamela <jonathanlinch7@gmail.com>
2022-05-09 17:55:28 -0300

    Fist commit

---
+++ src/com/company/Par_Impar.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*  Faça um programa que peça N números inteiros.
+    Calcule e mostre a quantidade de números pares
+    e a quantidade de números impares.*/
+public class Par_Impar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int valor;
+        int pares = 0;
+        int impares = 0;
+        int a;
+        int i = 0;
+        System.out.println("Quantos números quer testar?");
+        a = input.nextInt();
+        do {
+            System.out.println("Digite o numero: ");
+            valor = input.nextInt();
+            if (valor % 2 == 0){
+                pares++;
+            }else {
+                impares++;
+            }
+            i++;
+        } while (i < a);
+        System.out.println("Quantidade de números pares: " + pares);
+        System.out.println("Quantidade de números impares: " + impares);
+    }
+}
