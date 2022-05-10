320cb9a0d1fdfe4ec7a948b43f260d70f6535624
Jamela <jonathanlinch7@gmail.com>
2022-05-10 14:54:05 -0300

    Fist commit Matriz

---
+++ src/com/company/NumerosAleatorios.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Random;
+
+/* Faça um Programa que leia 20 números inteiros aleatórios
+    (entre 0 e 100) armazene-os num vetor.
+    Ao final, mostre os números e seus sucessores.*/
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        System.out.print("Números aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " " );
+        }
+
+        System.out.print("\nSucessor dos aleatorios: ");
+        for ( int numero : numerosAleatorios ) {
+            System.out.print((numero + 1)+ " " );
+        }
+    }
+}
